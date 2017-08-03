469a069ed6a8a1a8a2eb2e4cc28babe19b79d288
Kryolith <ben.binxia@gmail.com>
2017-08-03 15:57:30 +0800

    更新密码和封面

--- js/common.js
+++ js/common.js
@@ -1,2 +1,3 @@
-host = "../";
-// host = "http://users.cis.fiu.edu/~bxia/InformationSecurity/";
+// host = "/a/bear.cs.fiu.edu./disk/bear-d/users/taoli/public_html/security-book/";
+host = "http://users.cis.fiu.edu/~bxia/security-book/";
+// host = "http://users.cis.fiu.edu/~taoli/security-book/";
